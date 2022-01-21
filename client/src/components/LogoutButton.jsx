690dcba552f6a3b9ea9d8d0ee3add9dced62ffb5
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2022-01-21 13:34:35 +0100

    add function to logout button

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Home from './Home';
 
 const LogoutButton = () => {
+    const [disconnect, setDisconnect] = useState(false)
+
+    const handleLogoutSubmit = (e) => {
+        e.preventDefault();
+        setDisconnect(true)
+    }
+
+    if (disconnect) {
+        return (
+            <Home />
+        )
+    }
+
+
     return (
         <>
-            <button> logout </button>
+            <form onSubmit={handleLogoutSubmit}>
+                <button> logout </button>
+            </form>
         </>
-    );
-};
+    )
+}
 
 export default LogoutButton;
