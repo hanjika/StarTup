0e971cce27447febf9c1e01ff19dcaae4be87786
hanjika <hanna.masanjika@gmail.com>
2022-01-21 11:04:18 +0100

    Transfer user id from back end to front end

---
+++ client/src/components/Conversations.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Conversations = ({ id }) => {
+  return (
+    <section className='conversation-section'>
+        <h2>Your conversations {id}
+        </h2>
+    </section>
+  );
+};
+
+export default Conversations;
