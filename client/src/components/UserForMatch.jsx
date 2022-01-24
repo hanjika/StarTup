3ef4c6c22bbab6218a05fed9834d3ea6c7961790
hanjika <hanna.masanjika@gmail.com>
2022-01-24 17:12:47 +0100

    Initiate Tinder cards

--- client/src/components/UserForMatch.jsx
+++ client/src/components/UserForMatch.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import TinderCard from 'react-tinder-card';
 
 const UserForMatch = ({ user }) => {
-  return (
-      <div className='user'>
-          <h3>{user.first_name}</h3>
-          <h4>{user.starsign}</h4>
-          <p>{user.motto}</p>
-      </div>
-  );
+    const onSwipe = (direction) => {
+        console.log('You swiped: ' + direction)
+    }
+    
+    const onCardLeftScreen = (myIdentifier) => {
+        console.log(myIdentifier + ' left the screen')
+    }
+
+    return (
+        <TinderCard key={user.id} onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>
+            <div style={{backgroundImage: `url(${user.photo})`}} className='user-card'>
+                <h3>{user.first_name}</h3>
+                <h4>{user.starsign}</h4>
+                <p>{user.motto}</p>
+            </div>
+        </TinderCard>
+    );
 };
 
 export default UserForMatch;
