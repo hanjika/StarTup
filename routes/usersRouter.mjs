360b1b73793e699b8c1afe5a6a79f84bb08822e5
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2022-01-27 11:42:29 +0100

    display conversations into api

---
+++ routes/usersRouter.mjs
@@ -0,0 +1,16 @@
+import express from 'express'
+import { createRequire } from "module"
+const require = createRequire(import.meta.url);
+const users = require('../users.json')
+
+const usersRouter = express.Router()
+usersRouter.get('/api/users', function (req, res, next) {
+    res.status(200).json(users)
+})
+usersRouter.get('/api/users/:id', function (req, res) {
+    const id = req.params.id
+    const user = users.find(user => user.id === id)
+    res.status(200).json(user)
+})
+
+export default usersRouter
